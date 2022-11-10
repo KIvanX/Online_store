540ce2690cb185faedbd1e8fa61d6dfac99468fa
KIVAN <kosaci74@gmail.com>
2022-11-10 18:45:51 +0300

    commit

--- database.py
+++ database.py
@@ -11,30 +11,60 @@ class Database:
             port=5432
         )
         self.cur = self.con.cursor()
+        self.con.autocommit = True
 
-    def select(self, country=None, min_rating=0, film_id=None):
-        if film_id is not None:
-            self.cur.execute(f"SELECT * FROM films WHERE id = {film_id};")
-        elif country is not None:
-            self.cur.execute(f"SELECT * FROM films "
-                             f"WHERE rating >= {min_rating} and country = '{country}';")
+    # ______________________________________USERS____________________________________________________________
+
+    def create_table_user(self):
+        self.cur.execute('CREATE TABLE users('
+                         'id serial not null primary key,'
+                         'name varchar(64) not null ,'
+                         'password varchar(16) not null ,'
+                         'email varchar(64) not null );')
+
+    def add_user(self, user):
+        self.cur.execute('INSERT INTO users VALUES(default, %s, %s, %s) RETURNING id',
+                         (user['name'], user['password'], user['email']))
+        return self.cur.fetchone()[0]
+
+    def get_user(self, email, password):
+        if not password:
+            self.cur.execute('SELECT id FROM users WHERE email=%s', (email,))
         else:
-            self.cur.execute(f"SELECT * FROM films WHERE rating >= {min_rating};")
+            self.cur.execute('SELECT * FROM users WHERE email=%s AND password=%s', (email, password))
+        line = self.cur.fetchone()
+        return {'id': line[0], 'name': line[1], 'password': line[2], 'email': line[3]} if line else {}
 
-        data = self.prepare_data(self.cur.fetchall())
+    def edit_user(self, user_id, user):
+        self.cur.execute('UPDATE users SET name=%s, password=%s, email=%s WHERE id = %s',
+                         (user['name'], user['password'], user['email'], user_id))
 
-        return data
+    # ______________________________________PRODUCT____________________________________________________________
 
-    def insert(self, new_film):
-        self.cur.execute(f"INSERT INTO films VALUES(default, %s, %s, %s)",
-                         (new_film['name'], new_film['rating'], new_film['country']))
-        self.con.commit()
+    def create_table_product(self):
+        self.cur.execute('CREATE TABLE product('
+                         'id serial not null primary key,'
+                         'name varchar(128),'
+                         'price float,'
+                         'about varchar(2048));')
 
-    def prepare_data(self, data):
-        films = []
-        if len(data):
-            column_names = [desc[0] for desc in self.cur.description]
-            for row in data:
-                films += [{c_name: row[key] for key, c_name in enumerate(column_names)}]
+    def add_product(self, p):
+        self.cur.execute('INSERT INTO product VALUES(default, %s, %s, %s) RETURNING id',
+                         (p['name'], p['price'], p['about']))
+        return self.cur.fetchone()[0]
 
-        return films
+    def get_product(self, product_id=None):
+        self.cur.execute('SELECT * FROM product ORDER BY id')
+        products = []
+        for line in self.cur.fetchall():
+            products.append({'id': line[0], 'name': line[1], 'price': line[2], 'about': line[3]})
+            if line[0] == product_id:
+                return products[-1]
+        return products
+
+    def edit_product(self, product_id, product):
+        self.cur.execute('UPDATE product SET name=%s, price=%s, about=%s WHERE id = %s',
+                         (product['name'], product['price'], product['about'], product_id))
+
+    def delete_product(self, product_id):
+        self.cur.execute('DELETE FROM product WHERE id = %s', (product_id,))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, make_response, session
+from flask import Flask, render_template, request, redirect, make_response, session, flash
 from database import Database
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'fbRf43sRg4e3R3ke'
@@ -7,19 +8,115 @@ db = Database()
 
 
 @app.route("/")
-def films_list():
-    if not session.get('logged'):
-        return redirect("/authorization/")
+def index():
+    if 'OE_user' in session:
+        return render_template("index.html", products=db.get_product(), user=session['OE_user'])
+    else:
+        return redirect("authorization")
 
-    print(request.args)
 
-
-@app.route("/authorization/", methods=['POST', 'GET'])
-def new_film():
+@app.route("/authorization", methods=['POST', 'GET'])
+def authorization():
     if request.method == 'POST':
-        print(request.args)
+        user = db.get_user(request.form['email'], request.form['password'])
+        if user:
+            session['OE_user'] = user
+        return redirect("/")
+    elif request.args:
+        if not request.args['email']:
+            return {'status': 'error', 'message': 'Введите email'}
+        if not request.args['password']:
+            return {'status': 'error', 'message': 'Ведите пароль'}
 
+        user = db.get_user(request.args['email'], request.args['password'])
+        return {'status': 'ok'} if user else {'status': 'error', 'message': 'Пользователь не найден'}
     return render_template("authorization.html")
+
+
+@app.route("/registration", methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        user = {'name': request.form.get('name'),
+                'password': request.form.get('password'),
+                'email': request.form.get('email')}
+        user['id'] = db.add_user(user)
+        session['OE_user'] = user
+        return redirect("/")
+    elif request.args:
+        if not request.args['name']:
+            return {'status': 'error', 'message': 'Введите своё имя'}
+        if not request.args['email'] or request.args['email'].count('@') != 1 or request.args['email'].count('.') != 1:
+            return {'status': 'error', 'message': 'Некорректный email'}
+        if db.get_user(request.args['email'], ''):
+            return {'status': 'error', 'message': 'Пользователь с таким email уже существует'}
+        if len(request.args['password']) < 6:
+            return {'status': 'error', 'message': 'Пароль слишком короткий'}
+        if request.args['password'] != request.args['password2']:
+            return {'status': 'error', 'message': 'Пароли не совпадают'}
+        if request.args['checkbox'] == 'false':
+            return {'status': 'error', 'message': 'Необходимо принять правила сайта'}
+        return {'status': 'ok'}
+    return render_template("registration.html")
+
+
+@app.route("/profile", methods=['POST', 'GET'])
+def profile():
+    if request.method == 'POST':
+        user = {'id': session['OE_user']['id'],
+                'name': request.form.get('name'),
+                'password': request.form.get('password'),
+                'email': request.form.get('email')}
+        db.edit_user(session['OE_user']['id'], user)
+        session['OE_user'] = user
+        return redirect("/")
+
+    return render_template("profile.html", user=session.get('OE_user'))
+
+
+@app.route("/add_product", methods=['POST', 'GET'])
+def add_product():
+    if request.method == 'POST':
+        product = {'name': request.form.get('name'),
+                   'price': request.form.get('price'),
+                   'about': request.form.get('about')}
+        product_id = db.add_product(product)
+        request.files.get('photo').save(f'static/product{product_id}.jpg')
+        
+        return redirect('/')
+
+    return render_template("add_product.html")
+
+
+@app.route("/edit_product/<int:product_id>", methods=['POST', 'GET'])
+def edit_product(product_id):
+    if request.method == 'POST':
+        product = {'id': product_id,
+                   'name': request.form.get('name'),
+                   'price': request.form.get('price'),
+                   'about': request.form.get('about')}
+        db.edit_product(product_id, product)
+        if request.files['photo']:
+            request.files.get('photo').save(f'static/product{product_id}.jpg')
+
+        return redirect('/')
+
+    return render_template("edit_product.html", product=db.get_product(product_id=product_id))
+
+
+@app.route("/delete_product/<int:product_id>")
+def delete_product(product_id):
+    db.delete_product(product_id)
+    return redirect('/')
+
+
+@app.route("/buy_product/<int:product_id>", methods=['POST', 'GET'])
+def buy_product(product_id):
+    return render_template("edit_product.html", product=db.get_product(product_id=product_id))
+
+
+@app.route("/like_product/<int:product_id>", methods=['POST', 'GET'])
+def like_product(product_id):
+    return render_template("edit_product.html", product=db.get_product(product_id=product_id))
 
 
 if __name__ == "__main__":

9dd6080f3a5679bc6d3213b460e1a9fd37c6d380
KIVAN <kosaci74@gmail.com>
2022-11-13 13:18:42 +0300

    commit

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import datetime
 import psycopg2
 
 
@@ -49,22 +50,63 @@ class Database:
                          'about varchar(2048));')
 
     def add_product(self, p):
-        self.cur.execute('INSERT INTO product VALUES(default, %s, %s, %s) RETURNING id',
-                         (p['name'], p['price'], p['about']))
+        self.cur.execute('INSERT INTO product VALUES(default, %s, %s, %s, %s) RETURNING id',
+                         (p['name'], p['price'], p['about'], p['category']))
         return self.cur.fetchone()[0]
 
     def get_product(self, product_id=None):
         self.cur.execute('SELECT * FROM product ORDER BY id')
         products = []
         for line in self.cur.fetchall():
-            products.append({'id': line[0], 'name': line[1], 'price': line[2], 'about': line[3]})
+            products.append({'id': line[0], 'name': line[1], 'price': line[2], 'about': line[3], 'category': line[4]})
             if line[0] == product_id:
                 return products[-1]
         return products
 
     def edit_product(self, product_id, product):
-        self.cur.execute('UPDATE product SET name=%s, price=%s, about=%s WHERE id = %s',
-                         (product['name'], product['price'], product['about'], product_id))
+        self.cur.execute('UPDATE product SET name=%s, price=%s, category=%s, about=%s WHERE id = %s',
+                         (product['name'], product['price'], product['category'], product['about'], product_id))
 
     def delete_product(self, product_id):
         self.cur.execute('DELETE FROM product WHERE id = %s', (product_id,))
+
+    # ______________________________________LIKES_BASKETS_ORDERS____________________________________________________________
+    def add_to_favourites(self, user_id, product_id):
+        self.cur.execute('INSERT INTO favourites VALUES(%s, %s)', (user_id, product_id))
+
+    def get_from_favourites(self, user_id):
+        self.cur.execute('SELECT * FROM favourites WHERE user_id=%s', (user_id,))
+        return [e[1] for e in self.cur.fetchall()]
+
+    def delete_from_favourites(self, user_id, product_id):
+        self.cur.execute('DELETE FROM favourites WHERE user_id=%s AND product_id=%s', (user_id, product_id))
+
+    def add_to_basket(self, user_id, product_id):
+        self.cur.execute('INSERT INTO basket VALUES(%s, %s)', (user_id, product_id))
+
+    def get_from_basket(self, user_id):
+        self.cur.execute('SELECT * FROM basket WHERE user_id=%s', (user_id,))
+        return [e[1] for e in self.cur.fetchall()]
+
+    def delete_from_basket(self, user_id, product_id):
+        self.cur.execute('DELETE FROM basket WHERE user_id=%s AND product_id=%s', (user_id, product_id))
+
+    def make_order(self, user_id, products_id):
+        price = sum([self.get_product(product_id)['price'] for product_id in products_id])
+        date = datetime.datetime.now().strftime("%m.%d.%Y, %H:%M")
+        self.cur.execute('INSERT INTO orders VALUES(default, %s, %s, %s) RETURNING order_id', (user_id, price, date))
+        order_id = self.cur.fetchone()[0]
+        for product_id in products_id:
+            self.cur.execute('INSERT INTO order_products VALUES(%s, %s)', (order_id, product_id))
+
+    def get_orders_id(self, user_id):
+        self.cur.execute('SELECT order_id FROM orders WHERE user_id=%s', (user_id,))
+        return self.cur.fetchall()
+
+    def get_order(self, order_id):
+        self.cur.execute('SELECT * FROM orders WHERE order_id=%s', (order_id,))
+        line = self.cur.fetchone()
+        order = {'order_id': line[0], 'user_id': line[1], 'price': line[2], 'date': line[3]}
+        self.cur.execute('SELECT product_id FROM order_products WHERE order_id=%s', (order_id,))
+        order['products'] = [self.get_product(line[0]) for line in self.cur.fetchall()]
+        return order
--- main.py
+++ main.py
@@ -7,12 +7,52 @@ app.secret_key = 'fbRf43sRg4e3R3ke'
 db = Database()
 
 
-@app.route("/")
+def index_by_filter(product_type=''):
+    my_filter = {'min_price': int(request.form['min_price']) if request.form.get('min_price') else 0,
+                 'max_price': int(request.form['max_price']) if request.form.get('max_price') else 0,
+                 'find_word': request.form.get('find_word', ''),
+                 'category': product_type}
+
+    user = session['OE_user'] if 'OE_user' in session else {'id': 0, 'name': 'Гость', 'email': ''}
+    return render_template("index.html", products=db.get_product(), user=user, filter=my_filter,
+                           basket=db.get_from_basket(user['id']), favourites=db.get_from_favourites(user['id']))
+
+
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    if 'OE_user' in session:
-        return render_template("index.html", products=db.get_product(), user=session['OE_user'])
-    else:
-        return redirect("authorization")
+    return index_by_filter()
+
+
+@app.route("/category/<string:product_type>", methods=['POST', 'GET'])
+def category(product_type):
+    return index_by_filter(product_type)
+
+
+@app.route("/basket")
+def basket():
+    products_id = db.get_from_basket(session['OE_user']['id'])
+    user_products = [p for p in db.get_product() if p['id'] in products_id]
+    full_price = sum([p['price'] for p in user_products])
+    return render_template("basket.html", user_products=user_products, user=session['OE_user'], full_price=full_price)
+
+
+@app.route("/delete_from_basket/<int:product_id>")
+def delete_from_basket(product_id):
+    db.delete_from_basket(session['OE_user']['id'], product_id)
+    return redirect('/basket')
+
+
+@app.route("/favourites")
+def favourites():
+    products_id = db.get_from_favourites(session['OE_user']['id'])
+    user_products = [p for p in db.get_product() if p['id'] in products_id]
+    return render_template("favourites.html", user_products=user_products, user=session['OE_user'])
+
+
+@app.route("/delete_from_favourites/<int:product_id>")
+def delete_from_favourites(product_id):
+    db.delete_from_favourites(session['OE_user']['id'], product_id)
+    return redirect('/favourites')
 
 
 @app.route("/authorization", methods=['POST', 'GET'])
@@ -41,6 +81,7 @@ def registration():
                 'email': request.form.get('email')}
         user['id'] = db.add_user(user)
         session['OE_user'] = user
+        flash('Добро пожаловать на сайт OzonExpress')
         return redirect("/")
     elif request.args:
         if not request.args['name']:
@@ -68,9 +109,16 @@ def profile():
                 'email': request.form.get('email')}
         db.edit_user(session['OE_user']['id'], user)
         session['OE_user'] = user
+        flash('Профиль изменен')
         return redirect("/")
 
-    return render_template("profile.html", user=session.get('OE_user'))
+    orders = [db.get_order(order_id) for order_id in db.get_orders_id(session['OE_user']['id'])]
+    return render_template("profile.html", user=session.get('OE_user'), orders=orders)
+
+
+@app.route("/product/<int:product_id>")
+def product(product_id):
+    return render_template('product.html', user=session.get('OE_user'), product=db.get_product(product_id))
 
 
 @app.route("/add_product", methods=['POST', 'GET'])
@@ -78,10 +126,12 @@ def add_product():
     if request.method == 'POST':
         product = {'name': request.form.get('name'),
                    'price': request.form.get('price'),
+                   'category': request.form.get('category'),
                    'about': request.form.get('about')}
         product_id = db.add_product(product)
         request.files.get('photo').save(f'static/product{product_id}.jpg')
-        
+
+        flash('Товар добавлен')
         return redirect('/')
 
     return render_template("add_product.html")
@@ -89,15 +139,18 @@ def add_product():
 
 @app.route("/edit_product/<int:product_id>", methods=['POST', 'GET'])
 def edit_product(product_id):
+    global message
     if request.method == 'POST':
         product = {'id': product_id,
                    'name': request.form.get('name'),
                    'price': request.form.get('price'),
+                   'category': request.form.get('category'),
                    'about': request.form.get('about')}
         db.edit_product(product_id, product)
         if request.files['photo']:
             request.files.get('photo').save(f'static/product{product_id}.jpg')
 
+        flash('Товар успешно изменен')
         return redirect('/')
 
     return render_template("edit_product.html", product=db.get_product(product_id=product_id))
@@ -106,17 +159,51 @@ def edit_product(product_id):
 @app.route("/delete_product/<int:product_id>")
 def delete_product(product_id):
     db.delete_product(product_id)
+    flash('Товар удален')
     return redirect('/')
 
 
-@app.route("/buy_product/<int:product_id>", methods=['POST', 'GET'])
-def buy_product(product_id):
-    return render_template("edit_product.html", product=db.get_product(product_id=product_id))
+@app.route("/add_to_basket")
+def add_to_basket():
+    if 'OE_user' not in session:
+        return {'status': 'warning', 'message': 'Необходимо авторизоваться, чтобы купить этот товар'}
+    if int(request.args['product_id']) in db.get_from_basket(session['OE_user']['id']):
+        return {'status': 'warning', 'message': 'Этот товар уже в корзине'}
+
+    db.add_to_basket(session['OE_user']['id'], request.args['product_id'])
+    return {'status': 'success', 'message': 'Добавлено в корзину'}
 
 
-@app.route("/like_product/<int:product_id>", methods=['POST', 'GET'])
-def like_product(product_id):
-    return render_template("edit_product.html", product=db.get_product(product_id=product_id))
+@app.route("/add_to_favourites")
+def add_to_favourites():
+    if 'OE_user' not in session:
+        return {'status': 'warning', 'message': 'Необходимо авторизоваться, чтобы добавить в Избранное этот товар'}
+    if int(request.args['product_id']) in db.get_from_favourites(session['OE_user']['id']):
+        return {'status': 'warning', 'message': 'Этот товар уже в Избранном'}
+
+    db.add_to_favourites(session['OE_user']['id'], request.args['product_id'])
+    return {'status': 'success', 'message': 'Добавлено в Избранное'}
+
+
+@app.route("/make_order")
+def make_order():
+    products_id = db.get_from_basket(session['OE_user']['id'])
+    db.make_order(session['OE_user']['id'], products_id)
+    for product_id in products_id:
+        db.delete_from_basket(session['OE_user']['id'], product_id)
+    flash('Заказ принят')
+    return redirect('/')
+
+
+@app.route("/order/<int:order_id>")
+def order(order_id):
+    order_data = db.get_order(order_id)
+    return render_template("order.html", user=session['OE_user'], order=order_data)
+
+
+# @app.errorhandler(Exception)
+# def handle_exception(error):
+#     return render_template("error.html", error=error)
 
 
 if __name__ == "__main__":
